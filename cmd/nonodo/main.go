fed5bb5131def4765b7fb504c4b569a77b9fd6f1
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-12-11 11:53:39 -0300

    feat: setup cobra to parse options

--- cmd/nonodo/main.go
+++ cmd/nonodo/main.go
@@ -1,16 +1,42 @@
 // Copyright (c) Gabriel de Quadros Ligneul
 // SPDX-License-Identifier: Apache-2.0 (see LICENSE)
 
+// This package contains the nonodo binary.
+// To read the usage, run the binary passing the -h flag.
 package main
 
 import (
-	"context"
+	"os"
 
 	"github.com/gligneul/nonodo/internal/nonodo"
 	"github.com/gligneul/nonodo/internal/opts"
+	"github.com/spf13/cobra"
 )
 
+var cmd = &cobra.Command{
+	Use:   "nonodo",
+	Short: "Development Node for Cartesi Rollups",
+	Run:   run,
+}
+
+var nonodoOpts = opts.NewNonodoOpts()
+
+func init() {
+	cmd.Flags().IntVar(&nonodoOpts.AnvilBlockTime, "anvil-block-time",
+		nonodoOpts.AnvilBlockTime, "Time in seconds between Anvil blocks")
+	cmd.Flags().IntVar(&nonodoOpts.AnvilPort, "anvil-port", nonodoOpts.AnvilPort,
+		"HTTP port used by Anvil")
+	cmd.Flags().BoolVar(&nonodoOpts.AnvilVerbose, "anvil-verbose", nonodoOpts.AnvilVerbose,
+		"If true, prints Anvil's output")
+}
+
+func run(cmd *cobra.Command, args []string) {
+	nonodo.Run(cmd.Context(), nonodoOpts)
+}
+
 func main() {
-	opts := opts.NewNonodoOpts()
-	nonodo.Run(context.Background(), opts)
+	err := cmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
